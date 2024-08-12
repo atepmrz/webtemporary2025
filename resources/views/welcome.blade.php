0b5dfbca342c6975e61d7aa660d18f9fc7a927c0
Ramon Shabo <ramonwin@programmer.net>
2024-08-12 07:34:03 +0700

    WELCOME

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel 11 + livewire</title>
+    <title>Laravel + livewire</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
