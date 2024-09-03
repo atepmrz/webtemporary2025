79e388f65aa686cdad7d6107b1b2ae7b43fdb364
Ramon Shabo <ramonwin@programmer.net>
2024-09-03 21:30:26 +0700

    promo jsm

--- resources/views/main/promosi/proJsm.blade.php
+++ resources/views/main/promosi/proJsm.blade.php
@@ -37,7 +37,7 @@
                 </div>
             </div>
             <div class="col-md wow fadeInUp">
-                <img class="img-fluid" src="{{ asset('img/promo/promojsm.jpg') }}" alt="Promo JSM"
+                <img class="img-fluid" src="{{ asset('img/promo/jsm/jsm5.jpeg') }}" alt="Promo JSM"
                     style="border-radius: 15px;">
             </div>
         </div>
