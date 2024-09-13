cab83c4ecf7a2147d8c4cd2c6b90abfeb6aef61f
Ramon Shabo <ramonwin@programmer.net>
2024-09-13 09:31:16 +0700

    jsm 13-15

--- resources/views/main/promosi/proJsm.blade.php
+++ resources/views/main/promosi/proJsm.blade.php
@@ -53,7 +53,7 @@
 
         <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
 
-            @for ($x = 1; $x <= 8; $x++)
+            @for ($x = 1; $x <= 14; $x++)
                 <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
                     <img class="img-fluid rounded2" src="{{ asset('img/promo/jsm/jsm' . $x . '.jpeg') }}" alt="" />
                     <div class="project-title">
