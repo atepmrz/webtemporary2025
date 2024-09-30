155bfac60b2ed793d6057eee09ca0836b67ab332
Ramon Shabo <ramonwin@programmer.net>
2024-09-30 17:10:21 +0700

    iklan kat2024

--- resources/views/main/event/kat.blade.php
+++ resources/views/main/event/kat.blade.php
@@ -3,7 +3,7 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-2 mb-0 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-3 text-white animated slideInRight">Mekanisme program KAT 2024</h1>
+            <h1 class="display-3 text-white animated slideInRight">Cara Ikutan KAT 2024</h1>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb animated slideInRight mb-0">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
