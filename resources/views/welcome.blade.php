4418e7685fc077367d185e43a85bc0f2c31dbecf
Ramon Shabo <ramonwin@programmer.net>
2024-10-06 00:56:20 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel1234567891011</title>
+    <title>Laravel123456789101112</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
