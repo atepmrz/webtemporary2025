3943c090a746dd89c4b61b36edf0ec8668a30bbd
Ramon Shabo <ramonwin@programmer.net>
2024-10-07 08:55:24 +0700

    fashion video

--- resources/views/main/promosi/promoFashion.blade.php
+++ resources/views/main/promosi/promoFashion.blade.php
@@ -15,15 +15,15 @@
     </div>
     <!-- Page Header End -->
 
-    <section id="voucher" class="container-fluid">
+    <section id="voucher" class="container-fluid bg-warning">
         <div class="row p-4 g-4">
             <div class="col-md wow fadeInUp">
-                <video width="100%" controls style="width: 500px; height:500px">
+                <video class="rounded2" width="100%" controls style="width: 500px; height:500px">
                     <source src="{{ asset('video/batik.mp4') }}" type="video/mp4">
                 </video>
             </div>
             <div class="col-md wow fadeInUp">
-                <video width="100%" controls style="width: 500px; height:500px">
+                <video class="rounded2" width="100%" controls style="width: 500px; height:500px">
                     <source src="{{ asset('video/fashion1.mp4') }}" type="video/mp4">
                 </video>
             </div>
@@ -35,22 +35,4 @@
             border-radius: 10px;
         }
     </style>
-    <!-- Promosi -->
-    <div class="container-fluid pt-0 my-3 px-0">
-
-        <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
-
-            @for ($x = 1; $x <= 9; $x++)
-                <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
-                    <img class="img-fluid rounded2" src="{{ asset('img/promo/jsm/jsm' . $x . '.jpg') }}" alt="" />
-                    <div class="project-title">
-                        <button class="btn btn-light rounded-4 mb-0 text-primary lead">Beli Sekarang <i class="bi bi-cart"
-                                style="color: rgb(0, 180, 0)"></i>
-                        </button>
-                    </div>
-                </a>
-            @endfor
-        </div>
-    </div>
-    <!-- Project End -->
 @endsection
