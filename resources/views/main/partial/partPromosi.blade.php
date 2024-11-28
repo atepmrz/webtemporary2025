8c72c90c1a06cb30850f9e14acb784ee93850722
Ramon Shabo <ramonwin@programmer.net>
2024-11-28 08:23:05 +0700

    promo JSM 2224 dan promo gajian

--- resources/views/main/partial/partPromosi.blade.php
+++ resources/views/main/partial/partPromosi.blade.php
@@ -10,7 +10,7 @@
     </div>
     <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
 
-        @for ($x = 1; $x <= 24; $x++)
+        @for ($x = 1; $x <= 26; $x++)
             <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
                 <img class="img-fluid rounded2" src="{{ asset('img/promo/items/promo' . $x . '.jpg') }}"
                     alt="" />
