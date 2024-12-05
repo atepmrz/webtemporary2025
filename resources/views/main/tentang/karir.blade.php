188099e49601c2814cff4c3f5a22f0b6ddd49993
Ramon Shabo <ramonwin@programmer.net>
2024-12-05 09:08:03 +0700

    karir

--- resources/views/main/tentang/karir.blade.php
+++ resources/views/main/tentang/karir.blade.php
@@ -22,7 +22,7 @@
                         style="border-radius: 15px">
                 </div>
                 <br>
-                <a href="https://bit.ly/RekrutmenPramaBorma-Season2024" target="_blank" class="text-center lead"
+                <a href="https://bit.ly/RekrutmenPramaBorma-Des2024" target="_blank" class="text-center lead"
                     data-wow-delay="0.1s">
                     <i class="bi bi-journal-text"></i> Kirim CV Lamaran disini
                 </a>
@@ -33,8 +33,8 @@
                         style="border-radius: 15px">
                 </div>
                 <br>
-                <a href="https://bit.ly/RekrutmenPramaBorma-Okt2024" target="_blank" class="text-center lead"
-                    data-wow-delay="0.2s">
+                <a href="https://bit.ly/RekrutmenPramaBorma-Season2024" target="_blank" class="text-center lead"
+                    data-wow-delay="0.1s">
                     <i class="bi bi-journal-text"></i> Kirim CV Lamaran disini
                 </a>
             </div>
