fa00a4c02857e1fe2678db3e918a8edb42997e49
Ramon Shabo <ramonwin@programmer.net>
2024-12-06 16:32:16 +0700

    loker

--- resources/views/main/master_layout.blade.php
+++ resources/views/main/master_layout.blade.php
@@ -176,8 +176,9 @@
     <!-- Copyright Start -->
     <div class="container-fluid copyright bg-dark py-4">
         <div class="container text-center">
-            <p class="mb-2">Copyright &copy; 2024 <a class="fw-semi-bold" href="https://www.pramatoserba.co.id/"
-                    target="_blank">Prama Borma Toserba Group</a>, All Rights Reserved.</p>
+            <p class="mb-2">Copyright &copy; {{ date('Y') }} <a class="fw-semi-bold"
+                    href="https://www.pramatoserba.co.id/" target="_blank">Prama Borma Toserba Group</a>, All Rights
+                Reserved.</p>
             <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
             <p class="mb-0">Developed with <i class="bi bi-heart-fill text-danger"></i> By <a class="fw-semi-bold"
                     href="https://www.pramasolution.com/" target="_blank">pramasoftdev</a></p>
