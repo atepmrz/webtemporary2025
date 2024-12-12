79d7944df2e50778363cd8e7e8d8a408d03f0cc6
Ramon Shabo <ramonwin@programmer.net>
2024-12-12 09:17:53 +0700

    promo 12.12

--- resources/views/main/partial/partPromoSeasonal.blade.php
+++ resources/views/main/partial/partPromoSeasonal.blade.php
@@ -12,11 +12,11 @@
                             <div class="service-detail">
                                 <div class="service-title">
                                     <hr class="w-25">
-                                    <h3 class="mb-0">Promo JSM</h3>
+                                    <h3 class="mb-0">Promo 12.12</h3>
                                     <hr class="w-25">
                                 </div>
                             </div>
-                            <a class="btn btn-light" href="{{ url('/promo-jsm') }}">Promo JSM</a>
+                            <a class="btn btn-light" href="{{ url('/promo-jsm') }}">Promo 12.12</a>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
