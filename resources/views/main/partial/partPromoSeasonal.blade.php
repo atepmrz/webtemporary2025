31e27b6ea87da7ebf7f5db3a313f275c2f3e85af
Ramon Shabo <ramonwin@programmer.net>
2024-12-28 19:09:49 +0700

    malam tahun baru fix promo jsm & fmcg

--- resources/views/main/partial/partPromoSeasonal.blade.php
+++ resources/views/main/partial/partPromoSeasonal.blade.php
@@ -12,11 +12,11 @@
                             <div class="service-detail">
                                 <div class="service-title">
                                     <hr class="w-25">
-                                    <h3 class="mb-0">Promo Natal</h3>
+                                    <h3 class="mb-0">Promo JSM</h3>
                                     <hr class="w-25">
                                 </div>
                             </div>
-                            <a class="btn btn-light" href="{{ url('/promo-jsm') }}">Promo Natal</a>
+                            <a class="btn btn-light" href="{{ url('/promo-jsm') }}">Promo JSM</a>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -28,11 +28,11 @@
                             <div class="service-detail">
                                 <div class="service-title">
                                     <hr class="w-25">
-                                    <h3 class="mb-0">Promo Natal</h3>
+                                    <h3 class="mb-0">Grill Party</h3>
                                     <hr class="w-25">
                                 </div>
                             </div>
-                            <a class="btn btn-light" href="">Promo Natal</a>
+                            <a class="btn btn-light" href="">Grill Party</a>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -44,11 +44,11 @@
                             <div class="service-detail">
                                 <div class="service-title">
                                     <hr class="w-25">
-                                    <h3 class="mb-0">Promo Natal</h3>
+                                    <h3 class="mb-0">Grill Party</h3>
                                     <hr class="w-25">
                                 </div>
                             </div>
-                            <a class="btn btn-light" href="">Promo Natal</a>
+                            <a class="btn btn-light" href="">Grill Party</a>
                         </div>
                     </div>
                 </div>
