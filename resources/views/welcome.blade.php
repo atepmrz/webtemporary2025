e667208e8584cb54a2ac5a986bbfe683910cc4da
Ramon Shabo <ramonwin@programmer.net>
2025-01-10 10:32:18 +0700

    Promo JSM 101112

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel11 + mongodb</title>
+    <title>Laravel112 + mongodb</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
