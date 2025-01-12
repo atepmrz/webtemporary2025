cd7aeeecae84c3cc27d89adf1a1b1d14904f0a75
Ramon Shabo <ramonwin@programmer.net>
2025-01-13 01:03:23 +0700

    promo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel11234567891011234 + mongodb</title>
+    <title>Laravel112345678910112345 + mongodb</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
