bb105c8ab590aaaf0ae0eefe7e83ce6f96867b1b
Ramon Shabo <ramonwin@programmer.net>
2025-01-31 14:22:05 +0700

    fix : lokasi toko

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -274,7 +274,7 @@
                 <div class="col-md-6 col-xl-3 wow fadeIn" data-wow-delay="0.7s">
                     <div id="kotak" class="text-center border p-5">
                         <i class="fa fa-map-marker-alt fa-3x text-white mb-3"></i>
-                        <h5 class="display-2 text-primary mb-0" data-toggle="counter-up">12</h5>
+                        <h5 class="display-2 text-primary mb-0" data-toggle="counter-up">13</h5>
                         <span class="fs-5 fw-semi-bold text-white">Lokasi</span>
                     </div>
                 </div>
