2249e06d968a4eda470bc949a5d6fcb59c1f95b5
Ramon Shabo <ramonwin@programmer.net>
2025-02-05 10:21:12 +0700

    fix promo itme

--- resources/views/main/event/reguler.blade.php
+++ resources/views/main/event/reguler.blade.php
@@ -4,12 +4,12 @@
     <div class="container-fluid page-header py-2 mb-0 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <h1 class="display-3 text-white animated slideInRight">Reguler</h1>
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb animated slideInRight mb-0">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Event</a></li>
-                </ol>
-            </nav>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb animated slideInRight mb-0">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Event</a></li>
+                    </ol>
+                </nav>
         </div>
     </div>
     <!-- Page Header End -->
--- resources/views/main/tentang/karir.blade.php
+++ resources/views/main/tentang/karir.blade.php
@@ -57,11 +57,11 @@
                 })">
                 </div>
                 <br>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFqjQ-wKtv2oCSE3dHcMfZWz9dM4R68bg1xA6eyA0annFogA/viewform"
-                    target="_blank" class="text-center lead" data-wow-delay="0.2s">
-                    <i class="bi bi-journal-text"></i> Kirim CV Lamaran disini
-                </a>
-            </div>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfFqjQ-wKtv2oCSE3dHcMfZWz9dM4R68bg1xA6eyA0annFogA/viewform"
+                        target="_blank" class="text-center lead" data-wow-delay="0.2s">
+                        <i class="bi bi-journal-text"></i> Kirim CV Lamaran disini
+                    </a>
+                </div>
         </div>
         {{-- ============================================================= --}}
         <div class="row p-4 g-4">
