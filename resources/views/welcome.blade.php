4e5bc494be38edc11974f647dbaa6a63ddfe15b7
Ramon Shabo <ramonwin@programmer.net>
2025-02-06 11:27:57 +0700

    fix promo item

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel1123456 + mongodb</title>
+    <title>Laravel11234567 + mongodb</title>
 
     <!-- Fonts -->
 	<link rel="preconnect" href="https://fonts.bunny.net">
