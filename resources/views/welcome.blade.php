c2cda1f8dd98b3a6f87585e1332243c759796e1f
Ramon Shabo <ramonwin@programmer.net>
2025-02-21 14:18:00 +0700

    fix jumat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel1122345678910123456 + mongodb</title>
+    <title>Laravel11223456789101234567 + mongodb</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
