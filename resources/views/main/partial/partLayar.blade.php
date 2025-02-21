08615a6f4c36c8d723dcf8c35b0e1bed11e6e3e4
Ramon Shabo <ramonwin@programmer.net>
2025-02-21 14:00:26 +0700

    fix jumat

--- resources/views/main/partial/partLayar.blade.php
+++ resources/views/main/partial/partLayar.blade.php
@@ -283,7 +283,74 @@
                             </div>
                         </div>
                     </div>
-
+                </div>
+                <br>
+                 <div class="row g-4">
+                    <div class="col-lg-4 col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="team-item">
+                            <img class="img-fluid border-borma mb-2 border-borma"
+                                src="{{ asset('img/lokasi/tikma.jpg') }}" alt="" />
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 btn-square bg-primary" style="width: 90px; height: 90px">
+                                    <i class="fa fa-2x fa-share text-white"></i>
+                                </div>
+                                <div class="position-relative overflow-hidden bg-light d-flex flex-column justify-content-center w-100 ps-4"
+                                    style="height: 90px">
+                                    <a href="javascript:void(0)">
+                                        <h5>Tikma Soreang</h5>
+                                        <span class="text-primary"></span>
+                                        <div class="team-social">
+                                            <h4 class="text-white mb-0">New Store 🛒 </h4>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- <div class="col-lg col-md wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="team-item">
+                            <img class="img-fluid border-borma mb-2" src="{{ asset('img/lokasi/bbb.jpg') }}"
+                                alt="" />
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 btn-square bg-primary" style="width: 90px; height: 90px">
+                                    <i class="fa fa-2x fa-share text-white"></i>
+                                </div>
+                                <div class="position-relative overflow-hidden bg-light d-flex flex-column justify-content-center w-100 ps-4"
+                                    style="height: 90px">
+                                    <a href="https://wa.me/6282123767001/?text=" target="_blank">
+                                        <h5>Borma Buah Batu</h5>
+                                        <span class="text-primary"></span>
+                                        <div class="team-social">
+                                            <h3 class="text-white mb-0">Layar <i class="fab fa-whatsapp"
+                                                    style="color: green"></i></h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg col-md wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="team-item">
+                            <img class="img-fluid border-borma mb-2" src="{{ asset('img/lokasi/bcj.jpg') }}"
+                                alt="" />
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 btn-square bg-primary" style="width: 90px; height: 90px">
+                                    <i class="fa fa-2x fa-share text-white"></i>
+                                </div>
+                                <div class="position-relative overflow-hidden bg-light d-flex flex-column justify-content-center w-100 ps-4"
+                                    style="height: 90px">
+                                    <a href="https://wa.me/6282127416745/?text=" target="_blank">
+                                        <h5>Borma Cijerah</h5>
+                                        <span class="text-primary"></span>
+                                        <div class="team-social">
+                                            <h3 class="text-white mb-0">Layar <i class="fab fa-whatsapp"
+                                                    style="color: green"></i></h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
