e68441bdfa84a63149968b2871f5502cfa6045b3
Ramon Shabo <ramonwin@programmer.net>
2025-03-03 08:16:27 +0700

    fix pagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel112345678910123456 + mongodb</title>
+    <title>Laravel1123456789101234567 + mongodb</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
