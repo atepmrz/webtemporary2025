1b3e948fa31ec2b1a0c74277b9de3a192b3b762b
Ramon Shabo <ramonwin@programmer.net>
2025-03-07 15:33:31 +0700

    promo jsm ramadhan

--- resources/views/main/partial/partPromoSeasonal.blade.php
+++ resources/views/main/partial/partPromoSeasonal.blade.php
@@ -1,58 +1,58 @@
-    <!-- Service Start -->
-    <section id="seasonal" class="container-fluid">
-        <div class="container-xxl py-5">
-            <div class="container">
-                <div class="row gy-5 gx-4">
-                    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item">
+<!-- Service Start -->
+<section id="seasonal" class="container-fluid">
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row gy-5 gx-4">
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item">
+                        <img class="img-fluid" src="{{ asset('img/seasonal/1.jpg') }}" alt="">
+                        <div class="service-img">
                             <img class="img-fluid" src="{{ asset('img/seasonal/1.jpg') }}" alt="">
-                            <div class="service-img">
-                                <img class="img-fluid" src="{{ asset('img/seasonal/1.jpg') }}" alt="">
-                            </div>
-                            <div class="service-detail">
-                                <div class="service-title">
-                                    <hr class="w-25">
-                                    <h3 class="mb-0">Promo JSM</h3>
-                                    <hr class="w-25">
-                                </div>
-                            </div>
-                            <a class="btn btn-light" href="{{ url('/promo-jsm') }}">Promo JSM</a>
                         </div>
+                        <div class="service-detail">
+                            <div class="service-title">
+                                <hr class="w-25">
+                                <h3 class="mb-0">Promo JSM</h3>
+                                <hr class="w-25">
+                            </div>
+                        </div>
+                        <a class="btn btn-light" href="{{ url('/promo-jsm') }}">Promo JSM</a>
                     </div>
-                    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item">
+                </div>
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item">
+                        <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
+                        <div class="service-img">
                             <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
-                            <div class="service-img">
-                                <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
-                            </div>
-                            <div class="service-detail">
-                                <div class="service-title">
-                                    <hr class="w-25">
-                                    <h3 class="mb-0">Promo Fashion</h3>
-                                    <hr class="w-25">
-                                </div>
-                            </div>
-                            <a class="btn btn-light" href="">Promo Fashion</a>
                         </div>
+                        <div class="service-detail">
+                            <div class="service-title">
+                                <hr class="w-25">
+                                <h3 class="mb-0">Promo Fashion</h3>
+                                <hr class="w-25">
+                            </div>
+                        </div>
+                        <a class="btn btn-light" href="">Promo Fashion</a>
                     </div>
-                    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="service-item">
+                </div>
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item">
+                        <img class="img-fluid" src="{{ asset('img/seasonal/3.jpg') }}" alt="">
+                        <div class="service-img">
                             <img class="img-fluid" src="{{ asset('img/seasonal/3.jpg') }}" alt="">
-                            <div class="service-img">
-                                <img class="img-fluid" src="{{ asset('img/seasonal/3.jpg') }}" alt="">
-                            </div>
-                            <div class="service-detail">
-                                <div class="service-title">
-                                    <hr class="w-25">
-                                    <h3 class="mb-0">Promo Fashion</h3>
-                                    <hr class="w-25">
-                                </div>
-                            </div>
-                            <a class="btn btn-light" href="">Promo Fashion</a>
                         </div>
+                        <div class="service-detail">
+                            <div class="service-title">
+                                <hr class="w-25">
+                                <h3 class="mb-0">Promo Fashion</h3>
+                                <hr class="w-25">
+                            </div>
+                        </div>
+                        <a class="btn btn-light" href="">Promo Fashion</a>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- Service End -->
+    </div>
+</section>
+<!-- Service End -->
