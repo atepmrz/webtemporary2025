6bbd957420467ddc315c556c9c9565a4b27fa750
Ramon Shabo <ramonwin@programmer.net>
2025-03-25 11:36:13 +0700

    fix siang

--- resources/views/main/partial/partPromosi.blade.php
+++ resources/views/main/partial/partPromosi.blade.php
@@ -11,7 +11,7 @@
         </h1>
     </div>
     <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
-        @for ($x = 1; $x <= 26; $x++) <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
+        @for ($x = 1; $x <= 31; $x++) <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
             <img class="img-fluid rounded2" src="{{ asset('img/promo/items/promo' . $x . '.jpg') }}" alt="" />
             <div class="project-title">
                 <button class="btn btn-light rounded-4 mb-0 text-primary lead">Beli Sekarang <i class="bi bi-cart"
