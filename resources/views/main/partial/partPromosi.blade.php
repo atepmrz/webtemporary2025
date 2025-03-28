12f19740d005f12ea4b0fdd9a6bb9be749d882e8
Ramon Shabo <ramonwin@programmer.net>
2025-03-28 13:13:11 +0700

    promo fresh lebaran

--- resources/views/main/partial/partPromosi.blade.php
+++ resources/views/main/partial/partPromosi.blade.php
@@ -11,15 +11,16 @@
         </h1>
     </div>
     <div class="owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
-        @for ($x = 1; $x <= 20; $x++) <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
-            <img class="img-fluid rounded2" src="{{ asset('img/promo/items/promo' . $x . '.jpg') }}" alt="" />
-            <div class="project-title">
-                <button class="btn btn-light rounded-4 mb-0 text-primary lead">Beli Sekarang <i class="bi bi-cart"
-                        style="color: rgb(0, 180, 0)"></i>
-                </button>
-            </div>
+        @for ($x = 1; $x <= 19; $x++)
+            <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
+                <img class="img-fluid rounded2" src="{{ asset('img/promo/items/promo' . $x . '.jpg') }}" alt="" />
+                <div class="project-title">
+                    <button class="btn btn-light rounded-4 mb-0 text-primary lead">Beli Sekarang <i class="bi bi-cart"
+                            style="color: rgb(0, 180, 0)"></i>
+                    </button>
+                </div>
             </a>
-            @endfor
+        @endfor
     </div>
 </div>
 <!-- Project End -->
