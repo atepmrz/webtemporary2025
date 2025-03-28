f0239fe42bbbdfbffb6cd0be2696f155975c9572
Ramon Shabo <ramonwin@programmer.net>
2025-03-28 13:56:46 +0700

    demo masak & resep

--- resources/views/main/event/reguler.blade.php
+++ resources/views/main/event/reguler.blade.php
@@ -4,7 +4,7 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-2 mb-0 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-3 text-white animated slideInRight">Reguler</h1>
+            <h1 class="display-3 text-white animated slideInRight">Demo Masak & Resep Makanan</h1>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb animated slideInRight mb-0">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
