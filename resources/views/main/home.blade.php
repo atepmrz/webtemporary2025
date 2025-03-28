705c2ff9925dd4579e19e05d0ff6cb503a52b8ac
Ramon Shabo <ramonwin@programmer.net>
2025-03-28 13:35:26 +0700

    promo fresh lebaran

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -32,10 +32,19 @@
                 <a href="{{ url("/gebyar-lebaran") }}"><img src="{{ asset('img/banner/banner1.jpg') }}" class="d-block w-100"
                         alt="Image Slide"></a>
             </div>
-            @for ($x = 2; $x <= 6; $x++) <div class="carousel-item">
+            <div class="carousel-item">
+                <a href="{{ url("/promo-nugget") }}"><img src="{{ asset('img/banner/banner2.jpg') }}" class="d-block w-100"
+                        alt="Image Slide"></a>
+            </div>
+            <div class="carousel-item">
+                <a href="{{ url("/promo-hampers") }}"><img src="{{ asset('img/banner/banner3.jpg') }}" class="d-block w-100"
+                        alt="Image Slide"></a>
+            </div>
+            @for ($x = 4; $x <= 5; $x++)
+            <div class="carousel-item">
                 <img src="{{ asset('img/banner/banner' . $x . '.jpg') }}" class="d-block w-100" alt="Image Slide">
-        </div>
-        @endfor
+            </div>
+            @endfor
         {{-- <div class="carousel-item">
             <video width="1400px" height="500px" controls>
                 <source src="{{ asset('video/natal.mp4') }}" type="video/mp4">
