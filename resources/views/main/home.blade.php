e683bc1f4cb7f7d83e25d85c40e5d42b0da62a6f
Ramon Shabo <ramonwin@programmer.net>
2025-03-28 08:02:56 +0700

    promo fresh lebaran

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -85,7 +85,7 @@
                 <div class="row gx-5 h-500">
                     <div class="col-6 align-self-start wow fadeInUp" data-wow-delay="0.1s">
                         <img href="{{ url('/promo-jsm') }}" class="img-fluid border-3 border-primary"
-                            style="border-radius: 10px;" src="img/about-1.jpg" onclick="openImageModal.show({
+                            style="border-radius: 10px;" src="img/about-1.jpeg" onclick="openImageModal.show({
                                 img: this,
                                 animate: true
                             })" />
@@ -95,7 +95,7 @@
                     </div>
                     <div class="col-6 align-self-end wow fadeInDown" data-wow-delay="0.1s">
                         <img href="{{ url('/promo-jsm') }}" class="img-fluid border-3 border-primary"
-                            style="border-radius: 10px" src="img/about-2.jpg" onclick="openImageModal.show({
+                            style="border-radius: 10px" src="img/about-2.jpeg" onclick="openImageModal.show({
                                 img: this,
                                 animate: true
                             })" />
