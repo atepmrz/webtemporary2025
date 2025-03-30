88348934fd601188d2a2b049b6ae4355814e48cb
Ramon Shabo <ramonwin@programmer.net>
2025-03-30 23:01:48 +0700

    fix sahur

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel1123456789101212345 + mongodb</title>
+    <title>Laravel11234567891012123456 + mongodb</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
