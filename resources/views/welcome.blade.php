912e500e98d43b53c351f00b816f679121068941
ramonwin <ramonwin@programmer.net>
2025-04-10 21:03:14 +0700

    malam rumah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel1129012 + mongodb</title>
+    <title>Laravel11290123 + mongodb</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
