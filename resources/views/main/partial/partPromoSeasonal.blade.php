c6ecafd6aaf006fb06e705d0d073275cfe7309bc
ramonwin <ramonwin@programmer.net>
2025-04-22 03:48:11 +0700

    subuh

--- resources/views/main/partial/partPromoSeasonal.blade.php
+++ resources/views/main/partial/partPromoSeasonal.blade.php
@@ -5,6 +5,22 @@
             <div class="row gy-5 gx-4">
                 <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="service-item">
+                        <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
+                        <div class="service-img">
+                            <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
+                        </div>
+                        <div class="service-detail">
+                            <div class="service-title">
+                                <hr class="w-25">
+                                <h3 class="mb-0">Hari Kartini</h3>
+                                <hr class="w-25">
+                            </div>
+                        </div>
+                        <a class="btn btn-light" href="{{ url('/') }}">Kartini</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item">
                         <img class="img-fluid" src="{{ asset('img/seasonal/1.jpg') }}" alt="">
                         <div class="service-img">
                             <img class="img-fluid" src="{{ asset('img/seasonal/1.jpg') }}" alt="">
@@ -17,22 +33,6 @@
                             </div>
                         </div>
                         <a class="btn btn-light" href="{{ url('/promo-nugget') }}">Promo Nugget</a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="service-item">
-                        <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
-                        <div class="service-img">
-                            <img class="img-fluid" src="{{ asset('img/seasonal/2.jpg') }}" alt="">
-                        </div>
-                        <div class="service-detail">
-                            <div class="service-title">
-                                <hr class="w-25">
-                                <h3 class="mb-0">Promo Mudik</h3>
-                                <hr class="w-25">
-                            </div>
-                        </div>
-                        <a class="btn btn-light" href="{{ url('/mudik-seru') }}">Promo Mudik</a>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -51,6 +51,7 @@
                         <a class="btn btn-light" href="{{ url("/kue-sirup") }}">Kue & Sirup</a>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
