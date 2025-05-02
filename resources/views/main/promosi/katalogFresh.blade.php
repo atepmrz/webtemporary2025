5d8ac7fe8957d90d771036e3e609a5e9efc0d7b3
Ramon Shabo <ramonwin@programmer.net>
2025-05-02 11:12:19 +0700

    fix promo items expired

--- resources/views/main/promosi/katalogFresh.blade.php
+++ resources/views/main/promosi/katalogFresh.blade.php
@@ -3,7 +3,7 @@
 <section id="promo" class="container-fluid">
     <div class="container pt-3">
         <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-            @for ($x = 1; $x <= 12; $x++) <div class="testimonial-item align-items-center text-center">
+            @for ($x = 1; $x <= 10; $x++) <div class="testimonial-item align-items-center text-center">
                 <div class="position-relative">
                     <img style="max-height: 600px; max-width:400px" class="img-fluid mx-auto mb-5"
                         src="{{ asset('img/promo/fresh/' . $x . '.jpg') }}">
