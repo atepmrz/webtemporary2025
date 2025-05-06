e8b5538406fd1a2e8ba4e0e0b6b39c7b08804839
Ramon Shabo <ramonwin@programmer.net>
2025-05-06 09:09:34 +0700

    mailer prama borma reguler

--- resources/views/main/promosi/katalogBorma.blade.php
+++ resources/views/main/promosi/katalogBorma.blade.php
@@ -4,7 +4,7 @@
 <section id="promo" class="container-fluid">
     <div class="container pt-3">
         <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-            @for ($x = 1; $x <= 16; $x++) <div class="testimonial-item align-items-center text-center">
+            @for ($x = 1; $x <= 18; $x++) <div class="testimonial-item align-items-center text-center">
                 <div class="position-relative">
                     <img style="max-height: 600px; max-width:400px" class="img-fluid mx-auto mb-5"
                         src="{{ asset('img/promo/borma/' . $x . '.jpg') }}">
