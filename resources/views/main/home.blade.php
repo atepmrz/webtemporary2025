fb65b726a0a5223a8ca1c21d188fa069ecbbeb4f
Ramon Shabo <ramonwin@programmer.net>
2025-05-26 17:14:24 +0700

    idul adha promo

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -322,7 +322,7 @@
         <div class="modal-content">
             <div class="modal-body">
                 <a href="{{ url('/') }}">
-                    <img src="{{ asset('img/iklan/utama.jpg') }}" alt="Iklan" class="img-fluid rounded" />
+                    <img src="{{ asset('img/iklan/iduladha.jpeg') }}" alt="Iklan" class="img-fluid rounded" />
                 </a>
 
                 {{-- <video class="img-fluid rounded" width="100%" height="500px" controls>
