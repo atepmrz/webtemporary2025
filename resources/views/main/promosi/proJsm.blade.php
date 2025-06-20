e425624e92e137e11f00f8c3e3ee6d0952c2e491
Ramon Shabo <ramonwin@programmer.net>
2025-06-20 16:58:24 +0700

    fix sore hayu balik

--- resources/views/main/promosi/proJsm.blade.php
+++ resources/views/main/promosi/proJsm.blade.php
@@ -61,7 +61,7 @@
             </div>
         </div>
         <div class="col-md wow fadeInUp">
-            <img class="img-fluid" src="{{ asset('img/promo/jsm/jsm1.jpg') }}" alt="Promo JSM"
+            <img class="img-fluid" src="{{ asset('img/promo/jsm/jsm1.jpeg') }}" alt="Promo JSM"
                 style="border-radius: 15px;">
         </div>
     </div>
@@ -74,11 +74,10 @@
 </style>
 <!-- Promosi -->
 <div class="wow fadeInUp container-fluid pt-0 my-3 px-0" data-wow-delay="0.1s">
-
     <div class=" owl-carousel project-carousel wow fadeIn" data-wow-delay="0.1s">
 
-        @for ($x = 1; $x <= 25; $x++) <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
-            <img class="img-fluid rounded2" src="{{ asset('img/promo/jsm/jsm' . $x . '.jpg') }}" alt="" />
+        @for ($x = 1; $x <= 5; $x++) <a class="project-item pb-4 px-3" href="{{ url('/layar') }}"><br>
+            <img class="img-fluid rounded2" src="{{ asset('img/promo/jsm/jsm' . $x . '.jpeg') }}" alt="" />
             <div class="project-title">
                 <button class="btn btn-light rounded-4 mb-0 text-primary lead">Beli Sekarang <i class="bi bi-cart"
                         style="color: rgb(0, 180, 0)"></i>
